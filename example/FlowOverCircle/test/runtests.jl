8da5caf2298fba4de8c1bde09f9922597edd07f5
MilkshakeForReal <yicheng.wu@ucalgary.ca>
2022-05-31 01:34:54 -0600

    add test

--- example/FlowOverCircle/test/runtests.jl
+++ example/FlowOverCircle/test/runtests.jl
@@ -7,3 +7,9 @@ using Test
     loss = learner.cbstate.metricsepoch[ValidationPhase()][:Loss].values[end]
     @test loss < 0.1
 end
+
+@testset "Temporal Bulking" begin
+    learner = FlowOverCircle.train(K = 10; epochs=5)
+    loss = learner.cbstate.metricsepoch[ValidationPhase()][:Loss].values[end]
+    @test loss < 0.1
+end
